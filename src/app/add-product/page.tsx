53d152b0e9d4fead59b723c787c7b8ecad7e1b7a
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-08-05 22:08:40 +0200

    add product page server actions

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,70 @@
+import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
+
+export const metadata = {
+  title: "Add Product - ShopyBy",
+};
+
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Missing required fields");
+  }
+
+  await prisma.product.create({
+    data: { name, description, imageUrl, price },
+  });
+
+  redirect("/");
+}
+
+const AppProductPage = () => {
+  return (
+    <div>
+      <h1
+        className="mb-3 text-lg
+   font-bold"
+      >
+        AppProductPage
+      </h1>
+      <form action={addProduct}>
+        <input
+          required
+          name="name"
+          placeholder="name"
+          className="input input-bordered mb-3 w-full"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="description"
+          className="textarea textarea-bordered mb-3 w-full"
+        />
+        <input
+          required
+          name="imageUrl"
+          placeholder="image URL"
+          type="url"
+          className="input input-bordered mb-3 w-full"
+        />
+        <input
+          required
+          name="price"
+          placeholder="price"
+          type="number"
+          className="input input-bordered mb-3 w-full"
+        />
+        <button className="btn btn-primary btn-block" type="submit">
+          Add Product
+        </button>
+      </form>
+    </div>
+  );
+};
+export default AppProductPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
- 
-  )
+    <div>
+      <h1>main page</h1>
+    </div>
+  );
 }
