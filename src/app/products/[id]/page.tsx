7b8f852224c3e4278431b51294605d7bc7346c66
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-08-06 13:32:13 +0200

    product page specifics added

---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,52 @@
+import { prisma } from "@/lib/db/prisma";
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import PriceTag from "@/components/PriceTag";
+import { Metadata } from "next";
+import { cache } from "react";
+
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const getProduct = cache(async (id: string) => {
+  const product = await prisma.product.findUnique({ where: { id } });
+  if (!product) notFound();
+  return product;
+});
+
+export const generateMetadata = async ({
+  params: { id },
+}: ProductPageProps): Promise<Metadata> => {
+  const product = await getProduct(id);
+
+  return {
+    title: product.name + "- ShopiBy",
+    description: product.description,
+  };
+};
+
+const ProductPage = async ({ params: { id } }: ProductPageProps) => {
+  const product = await getProduct(id);
+
+  return (
+    <div className="flex flex-col gap-4 lg:flex-row lg:items-center">
+      <Image
+        src={product.imageUrl}
+        width={500}
+        height={500}
+        alt={product.name}
+        className="rounded-lg"
+        priority
+      />
+      <div>
+        <h1 className="text-5xl font-bold">{product.name}</h1>
+        <PriceTag price={product.price} className="mt-4" />
+        <p className="py-6">{product.description}</p>
+      </div>
+    </div>
+  );
+};
+export default ProductPage;
