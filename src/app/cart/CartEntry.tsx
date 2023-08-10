83291e9e9cccb98a257331d5aa3cff4f76fa9921
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-08-10 23:49:23 +0200

    CartEntry updated

--- src/app/cart/CartEntry.tsx
+++ src/app/cart/CartEntry.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { CartItemWithProduct } from "@/lib/db/cart";
+import { formatPrice } from "@/lib/db/format";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CartEntryProps {
   cartItem: CartItemWithProduct;
@@ -18,6 +20,15 @@ const CartEntry = ({ cartItem: { product, quantity } }: CartEntryProps) => {
           height={200}
           className="rounded-lg"
         />
+        <div>
+          <Link href={"/products/" + product.id} className="font-bold">
+            {product.name}
+          </Link>
+          <div>Price: {formatPrice(product.price)}</div>
+          <div className="flex items-center gap-3">
+            Total: {formatPrice(product.price * quantity)}
+          </div>
+        </div>
       </div>
       <div className="divider" />
     </div>
