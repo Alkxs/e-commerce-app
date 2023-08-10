ca6c131f3ff4124f4c85250eaace0c9d034643b1
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-08-11 00:49:39 +0200

    Cart Page completed

--- src/app/cart/CartEntry.tsx
+++ src/app/cart/CartEntry.tsx
@@ -4,15 +4,31 @@ import { CartItemWithProduct } from "@/lib/db/cart";
 import { formatPrice } from "@/lib/db/format";
 import Image from "next/image";
 import Link from "next/link";
+import { useTransition } from "react";
 
 interface CartEntryProps {
   cartItem: CartItemWithProduct;
+  setProductQuantity: (productId: string, quantity: number) => Promise<void>;
 }
 
-const CartEntry = ({ cartItem: { product, quantity } }: CartEntryProps) => {
+const CartEntry = ({
+  cartItem: { product, quantity },
+  setProductQuantity,
+}: CartEntryProps) => {
+  const [isPending, startTransition] = useTransition();
+
+  const quantityOptions: JSX.Element[] = [];
+  for (let i = 1; i <= 99; i++) {
+    quantityOptions.push(
+      <option value={i} key={i}>
+        {i}
+      </option>,
+    );
+  }
+
   return (
     <div>
-      <div className="itcem flex flex-wrap items-center gap-3">
+      <div className="flex flex-wrap items-center gap-3">
         <Image
           src={product.imageUrl}
           alt={product.name}
@@ -25,8 +41,27 @@ const CartEntry = ({ cartItem: { product, quantity } }: CartEntryProps) => {
             {product.name}
           </Link>
           <div>Price: {formatPrice(product.price)}</div>
+          <div className="my-1 flex items-center gap-2">
+            Quantity:
+            <select
+              className="select select-bordered w-full max-w-[80px]"
+              defaultValue={quantity}
+              onChange={(e) => {
+                const newQuantity = parseInt(e.currentTarget.value);
+                startTransition(async () => {
+                  await setProductQuantity(product.id, newQuantity);
+                });
+              }}
+            >
+              <option value={0}>0 (Remove)</option>
+              {quantityOptions}
+            </select>
+          </div>
           <div className="flex items-center gap-3">
             Total: {formatPrice(product.price * quantity)}
+            {isPending && (
+              <span className="loading loading-spinner loading-sm" />
+            )}
           </div>
         </div>
       </div>
